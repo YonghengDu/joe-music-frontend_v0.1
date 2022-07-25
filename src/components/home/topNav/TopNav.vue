4037142443fefbc9c622b6b76b81024b04564436
YonghengDu <15571795796@163.com>
2022-07-26 03:47:10 +0800

    v0.1_001-完成项目初始化，图标方案，Home界面导航

---
+++ src/components/home/topNav/TopNav.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="topNav">
+    <div class="navLeft">
+      <svg class="iconpark-icon"><use href="#search"></use></svg>
+    </div>
+    <div class="navContainer">
+      <span>我的</span>
+      <span class="active">发现</span>
+      <span>云村</span>
+      <span>视频</span>
+    </div>
+    <div class="navRight">
+      <svg class="iconpark-icon"><use href="#hamburger-button"></use></svg>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+    .topNav{
+        width: 100%;
+        padding: 10px;
+        display: flex;
+        justify-content:space-between;
+        align-items: center;
+    }
+    .navContainer span{
+        padding: 10px;
+    }
+    .active{
+        font-weight: 900;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TopNav />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+
+import TopNav from '@/components/home/topNav/TopNav.vue'
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    TopNav,
   },
 };
 </script>
